1e655b32771fe8fa63df2cd5b5d2eb6d572f4508
nandadeep <aditya.nandadeep@gmail.com>
2019-03-04 14:12:11 -0800

    added support for multiple match finding in kmp

---
+++ match-algorithms/bm.py
@@ -0,0 +1,4 @@
+class Boyer_Moore(object):
+	def __init__(self, pattern, text):
+		self.pattern = pattern; self.text = text
+		self.len_pattern = len(pattern); self.len_text = len(text)
--- match-algorithms/kmp.py
+++ match-algorithms/kmp.py
@@ -1,50 +1,62 @@
-import cProfile, tqdm
-# def pi_table(pattern):
-#     pi_len = len(pattern); pi = [0] * pi_len
-#     pi[0] = -1; k = -1
-#     for i in tqdm.tqdm(range(pi_len)):
-#         while (k >= 0) and (pattern[k + 1] != pattern[i]):
-#             k = pattern[k]
-#         k += 1; pi[i] = k
-#     return pi
-
 class KMP(object):
 	def __init__(self, pattern, text):
-		self.pattern = pattern; self.text = text
-		self.len_pattern = len(pattern); self.len_text = len(text)
-	
-	def kmp_search(self):
-		if self.pattern == "":
-			return 0  # Immediate match - base case 0
-		# print(len(text))
-		# Compute longest suffix-prefix table
-		lsp = [0]  # Base case 1
+		self.pat = pattern; self.txt = text
+		self.M = len(pattern); self.N = len(text)
+		self.matches = list()
 
-		for c in self.pattern[1 : ]:
-			j = lsp[-1]  # Start by assuming we're extending the previous LSP
-			while j > 0 and c != self.pattern[j]:
-				j = lsp[j - 1]
-			if c == self.pattern[j]:
+	def KMPSearch(self): 
+		j = 0 # index counter for pattern 
+		lps = self.computeLPSArray() # build the longest prefix table 
+		i = 0 # index counter for text
+		while i < self.N: # iterating throught the text
+			if self.pat[j] == self.txt[i]: # when there's a character match
+				i += 1
 				j += 1
-			lsp.append(j)
-		print("pi table: ", lsp)
-		j = 0  # Number of chars matched in pattern
-		for i in range(self.len_text):
-			while j > 0 and self.text[i] != self.pattern[j]:
-				j = lsp[j - 1]  # Fall back in the pattern
-			if self.text[i] == self.pattern[j]:
-				j += 1  # Next char matched, increment position
-				if j == self.len_pattern:
-					return i - (j - 1)
-		return None  # Not found
+
+			if j == self.M: 
+				# print("Found pattern at index " + str(i-j))
+				self.matches.append((i - j))
+				j = lps[j - 1] 
+
+			elif i < self.N and self.pat[j] != self.txt[i]: # mismatch after j matches
+				# Do not match lps[0..lps[j-1]] characters, 
+				# they will match anyway 
+				if j != 0: 
+					j = lps[j-1] 
+				else: 
+					i += 1
+		return self.matches
+
+	def computeLPSArray(self):
+        
+		lps = [0] * self.M
+		l = 0 # length of the previous longest prefix suffix 
+		lps[0] # lps[0] is always 0 
+		i = 1
+
+		# the loop calculates lps[i] for i = 1 to M-1 
+		while i < self.M: 
+			if self.pat[i]== self.pat[l]: 
+				l += 1
+				lps[i] = l
+				i += 1
+			else: 
+				# This is tricky. Consider the example. 
+				# AAACAAAA and i = 7. The idea is similar 
+				# to search step. 
+				if l != 0: 
+					l = lps[l - 1] 
+				else: 
+					lps[i] = 0
+					i += 1
+		return lps
+
 
 def main():
-	pattern = "going"
-	text = "How is it aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasfadsbgsdtzesdxndbrgdgvdxvffxdfgxdvfgxdcgxdgxfdgxdrfngsrdbgbvedgxetxdrtxdrtxdvvfgxdfgdxnhxdtxbdfgxdfgxfbgdzftbxrtdg going"
-	matcher = KMP(pattern, text)
-	print("found at : ", matcher.kmp_search())
-	# print(kmp_search("going", "How is it aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasfadsbgsdtzesdxndbrgdgvdxvffxdfgxdvfgxdcgxdgxfdgxdrfngsrdbgbvedgxetxdrtxdrtxdvvfgxdfgdxnhxdtxbdfgxdfgxfbgdzftbxrtdg going"))
-	# cProfile.run('kmp_search("going", "How is it aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasfadsbgsdtzesdxndbrgdgvdxvffxdfgxdvfgxdcgxdgxfdgxdrfngsrdbgbvedgxetxdrtxdrtxdvvfgxdfgdxnhxdtxbdfgxdfgxfbgdzftbxrtdg going")')
+    txt = "How is it aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasfadsbgsdgoingtzesdxndbrgdgvdxvffxdfgxdvfgxdcgxdgxfdgxdrfngsrdbgbvedgxetxdrtxdrtxdvvfgxdfgdxnhxdtxbdfgxdfgxfbgdzftbxrtdg going"
+    pat = "going"
+    kmp = KMP(pat, txt)
+    print(kmp.KMPSearch())
 
 if __name__ == "__main__":
-	main ()
+    main()
