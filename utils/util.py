82e098bd982847cf28d602e0fa44a59015767aeb
nandadeep <aditya.nandadeep@gmail.com>
2019-03-04 19:52:17 -0800

    adding plotting functions

--- utils/util.py
+++ utils/util.py
@@ -1,4 +1,5 @@
 import os, numpy
+from matplotlib import pyplot as plt
 
 class FileManipulation(object):
 
@@ -12,12 +13,24 @@ class FileManipulation(object):
     def read_file(self):
         with open(self.path, 'r') as f:
             self.content.extend(f.read().split("\n"))
+
+class Plotter(object):
+
+    def plot_text_time(self, experiment, times, lengths):
+        plt.plot(times, lengths)
+        plt.xlabel("Time"); plt.ylabel("Text Length")
+        plt.savefig("./metrics/" + str(experiment) + ".png")
+
+        
     
 def main():
     # a test driver 
-    path = "sample.txt"
-    fm = FileManipulation(path)
-    fm.read_file(); print(fm.get_file_contents())
+    # path = "sample.txt"
+    # fm = FileManipulation(path)
+    # fm.read_file(); print(fm.get_file_contents())
+
+    p = Plotter()
+    p.plot_text_time(2, [4, 5, 6, 7], [100, 200, 500, 600])
 
 if __name__ == "__main__":
     main()
